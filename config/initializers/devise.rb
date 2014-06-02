f4765a82be1966bc52d8840e1ff02ae2748d7a89
jmscholen <jmscholen@gmail.com>
2014-06-02 08:58:30 -0400

    updated config file to work with Heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -20,7 +20,7 @@ Devise.setup do |config|
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
   require 'devise/orm/active_record'
-
+  config.secret_key = '8fc50433ba83a4f9c1386a196b1053ba50d5ad404db65e9a4d8b610d679386f60257dac58bca79b31f963803899fa3b27e82a34ca04213aad5bf27ae3782f6c3'
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
   # just :email. You can configure it to use [:username, :subdomain], so for
