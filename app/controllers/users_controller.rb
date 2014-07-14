0d612cded5f89a047219bf6251aee6e08b648270
jmscholen <jmscholen@gmail.com>
2014-07-14 16:56:57 -0400

    minor correction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,3 @@
 class UsersController < ApplicationControllers
-	
-	# def create
-	# 	@new_user = User.new(user_params)
-	# 	@new_user.skip_confirmation!
-	# 	if @new_user.save
-	# 		redirect_to "/"
-	# 	else 
-	# 		redirect_to "/", notice: "There was a problems with your sign up!"
-	# 	end
-	# end
 
 end
